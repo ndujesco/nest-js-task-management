65036a3c7d029ed893b511fa4a978ada4fdae35d
ndujesco <ndujesco@users.noreply.github.com>
2023-07-03 21:20:07 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from "@nestjs/common";
 import { TaskModule } from "./tasks/tasks.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import {  typeOrmConfig } from "./config/typeorm.config";
 import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [TaskModule, ConfigModule.forRoot()]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TaskModule, 
+    TypeOrmModule.forRoot(typeOrmConfig)
+  ]
 })
+
+
 export class AppModule {}
