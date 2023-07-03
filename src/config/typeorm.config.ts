f55b693263d854392be89e5039cf5ad95a8e2238
ndujesco <ndujesco@users.noreply.github.com>
2023-07-03 18:50:04 +0100

    update

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,9 +1,11 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: 
-}
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: process.env.PASSWORD,
+  database: "taskmanagement",
+  entities: [__dirname + '/../**/*.entity.{js,ts}']
+};
