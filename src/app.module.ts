c053dea2c327b9b38338f89cbf2bebe9322e7474
ugo <ndujesco@users.noreply.github.com>
2023-07-12 01:26:21 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TaskModule } from "./tasks/tasks.module";
 
 // import { ConfigModule } from "@nestjs/config";
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TaskModule,
-    PrismaModule
+    PrismaModule,
+    UserModule
   ]
 })
 export class AppModule {}
