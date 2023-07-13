b8daaed8b0b5ad8abf1f69d405ef6a05dc950a34
ugo <ndujesco@users.noreply.github.com>
2023-07-13 12:33:33 +0100

    update

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async signup(authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
+    const user = await this.prismaService.user.create({data: {
+        username, password
+    }})
+  }
+}
