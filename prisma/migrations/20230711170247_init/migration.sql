451b4b756792bed613ce98c12b86305b58d07a81
ugo <ndujesco@users.noreply.github.com>
2023-07-14 11:29:09 +0100

    update

--- prisma/migrations/20230711170247_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Task` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `status` ENUM('OPEN', 'IN_PROGRESS', 'DONE') NOT NULL,
-    `title` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
