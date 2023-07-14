3bb4bf1ea2633a9d014d54e7be0daa0385861053
ugo <ndujesco@users.noreply.github.com>
2023-07-14 12:03:25 +0100

    update

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { ConflictException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { hash, compare } from "bcryptjs";
-import * as jwt from "jsonwebtoken";
 
 @Injectable()
 export class AuthService {
